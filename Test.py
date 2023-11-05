0574f569b58830db388df352b1a49854fdcf6df0
9galador0 <msncisco@hotmail.it>
2023-11-05 20:13:44 +0100

    adding files

---
+++ Test.py
@@ -0,0 +1,8 @@
+#Questo si può usare per eseguire il codice dal Terminal con degli input
+
+import sys
+first=sys.argv[1] #Carica il primo termine (in 0 c'è il nome del file)
+last=sys.argv[2]
+print(f'Ciao {first} {last}!')
+
+#per eseguire -> py Test.py Fra Ci
---
+++ Test1.py
@@ -0,0 +1,11 @@
+import pyjokes
+
+joke = pyjokes.get_joke('en', 'neutral')
+
+print(joke)
+
+#usare pip per vedere i pacchetti installati
+#per installare packages --> pip3 install pyjokes
+#Se no, non carica perchè il pacchetto può essere installato solo su pycharme o altro ma nn nel sistema
+#pip list mostra la lista dei pacchetti
+#pip -V indica la versione
